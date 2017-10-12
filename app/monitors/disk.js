99997f4408ac13fcb60ce51650985832576dfb31
Jay Reardon <jayreardon@gmail.com>
2017-10-12 13:30:03 -0600

    #9 -- Migrated disk usage to systeminformation library

--- app/monitors/disk.js
+++ app/monitors/disk.js
@@ -3,9 +3,7 @@
 let
   _             = require('../lib/lodashExt')
   , os          = require('os')
-  , bluebird    = require('bluebird')
-  , drivelist   = bluebird.promisifyAll(require('drivelist'))
-  , diskusage   = bluebird.promisifyAll(require('diskusage'))
+  , si          = require('systeminformation')
   , Monitor     = require('../lib/monitor')
 ;
 
@@ -16,58 +14,36 @@ class DiskMonitor extends Monitor {
   }
 
   collect() {
-    drivelist.listAsync()
-      .map((device) => {
-        let deviceMountPath = device.mountpoints && device.mountpoints[0] && device.mountpoints[0].path;
+    si.fsSize()
+    .then((diskStatisticsList) => {
+      let allStatistics = {};
+      let totalUsed = 0;
+      let total = 0;
 
-        if (deviceMountPath == null) { return; }
-
-        return diskusage.checkAsync(deviceMountPath)
-          .then((deviceInfo) => {
-            const deviceMountPathStatsdName = deviceMountPath.replace(':', '');
-            return {
-              'name': deviceMountPathStatsdName,
-              'free': deviceInfo.free,
-              'free_percent': (deviceInfo.free / deviceInfo.total) * 100,
-              'total': deviceInfo.total,
-              'used': (deviceInfo.total - deviceInfo.free),
-              'used_percent': ((deviceInfo.total - deviceInfo.free) / deviceInfo.total) * 100
-            };
-          })
-          .catch(err => {
-            this.log.error(err.stack || err);
-            return {};
-          });
-      })
-      .then((diskStatisticsList) => {
-        let allStatistics = {};
-        let totalFree = 0;
-        let total = 0;
-
-        for (let i = 0, j = diskStatisticsList.length; i < j; i++) {
-          let diskStatistics = diskStatisticsList[i];
-          if(_.hasValue(diskStatistics)) {
-            totalFree += diskStatistics.free;
-            total += diskStatistics.total;
-            let name = diskStatistics.name;
-            delete diskStatistics.name;
-            //allStatistics[name] = diskStatistics;
-          }
+      for (let i = 0, j = diskStatisticsList.length; i < j; i++) {
+        let diskStatistics = diskStatisticsList[i];
+        if(_.hasValue(diskStatistics)) {
+          totalUsed += diskStatistics.used;
+          total += diskStatistics.size;
+          let name = diskStatistics.fs;
+          delete diskStatistics.fs;
+          //allStatistics[name] = diskStatistics;
         }
+      }
 
-        allStatistics = {
-          'free': totalFree,
-          'free_percent': (totalFree / total) * 100,
-          'total': total,
-          'used': (total - totalFree),
-          'used_percent': ((total - totalFree) / total) * 100
-        }
+      allStatistics = {
+        'free': (total - totalUsed),
+        'free_percent': ((total - totalUsed) / total) * 100,
+        'total': total,
+        'used': totalUsed,
+        'used_percent': (totalUsed / total) * 100
+      }
 
-        this.setStats(allStatistics);
-      })
-      .catch((err) => {
-        this.log.error(err.stack || err);
-      })
+      this.setStats(allStatistics);
+    })
+    .catch((err) => {
+      this.log.error(err.stack || err);
+    })
   }
 }
 
