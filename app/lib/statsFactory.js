1317f8ceeb3453c66a9785156a1139d0a0d50c34
Jay Reardon <jayreardon@gmail.com>
2018-12-25 05:23:55 -0700

    #24 -- Add statistics factory

---
+++ app/lib/statsFactory.js
@@ -0,0 +1,61 @@
+// app/lib/statsFactory.js
+const __ = require('@mediaxpost/lodashext');
+
+class StatsFactory {
+  constructor(hostname, statsd, tags, log) {
+    this.hostname = hostname || 'localhost';
+    this.statsd = statsd;
+    this.log = log;
+    if (!tags) {
+      this.tags = [];
+    } else if (Array.isArray(tags)) {
+      // Make a copy of the tags array (the original array should remain immutable)
+      this.tags = Object.assign([], tags);
+    } else {
+      this.tags = [tags];
+    }
+    this.tags.push('serverName:' + this.hostname);
+  }
+
+  create(name, value) {
+    name = name.split('.')
+      .map((str) => {
+        return __.snakeCase(str);
+      })
+      .join('.');
+
+    return {
+      name: name,
+      value: value
+    };
+  }
+
+  send(collection) {
+    if (!Array.isArray(collection)) {
+      if (typeof collection !== 'object') {
+        throw new Error('Can not send a value')
+      } else {
+        collection = [collection];
+      }
+    }
+    // Only send if the statsd connection has not been shut down
+    if (__.hasValue(this.statsd)) {
+      for (let itr = 0, jtr = collection.length; itr < jtr; itr++) {
+        let stat = collection[itr]
+        this.log.silly('Sending statistic %s = %d', stat.name, stat.value);
+
+        if (typeof stat !== 'object' || !stat.hasOwnProperty('name') || !stat.hasOwnProperty('value')) {
+          continue;
+        }
+
+        this.statsd.gauge(stat.name, stat.value, this.tags, (error) => {
+          if (error) {
+            this.log.error(error.stack || error);
+          } else {
+            this.log.silly('Success');
+          }
+        });
+      }
+    }
+  }
+}
