50b782f3b01a4962a066acaaba0f7528ab02a75b
Jay Reardon <jayreardon@gmail.com>
2019-01-07 14:14:11 -0700

    #10 -- Fixing linting

--- test/monitors/system.js
+++ test/monitors/system.js
@@ -1,6 +1,5 @@
 // test/monitors/system.js
 
-const __ = require('@mediaxpost/lodashext');
 const chai = require('chai');
 const expect = chai.expect;
 const SystemMonitor = require('../../app/monitors/system');
